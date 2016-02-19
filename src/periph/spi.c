3fd623d21110d99c37c3ab520e7a79677c04ae09
kripton <brazzers1969@gmail.com>
2016-02-19 15:58:36 +0300

    Wrote SPI driver

--- src/periph/spi.c
+++ src/periph/spi.c
@@ -8,83 +8,160 @@
 #include <periph/spi.h>
 
 
-void SPI_init(SPI_TypeDef *SPI)
+#define SPI1_RX_BUFFER_SIZE 256
+#define SPI1_TX_BUFFER_SIZE 256
+
+static uint16_t spi_rx_buffer[SPI1_RX_BUFFER_SIZE];
+static uint16_t spi_tx_buffer[SPI1_TX_BUFFER_SIZE];
+
+static uint8_t rx_counter = 0;
+static uint8_t rx_wr_pointer = 0;
+static uint8_t rx_rd_pointer = 0;
+
+static uint8_t tx_counter = 0;
+static uint8_t tx_wr_pointer = 0;
+static uint8_t tx_rd_pointer = 0;
+
+
+void SPI1_init(void)
 {
-
-/*
-	SPI1->CR1 |=  SPI_CR1_BR_1;		//prescaler 8
-	SPI1->CR1 |=  SPI_CR1_SSM;		//Software slave management
-	SPI1->CR1 |=  SPI_CR1_SSI;		//NSS is HIGH
-	SPI1->CR1 |=  SPI_CR2_SSOE;		//NSS is slave select
-	SPI1->CR1 |=  SPI_CR1_MSTR;		//Master
-	SPI1->CR1 &=  ~SPI_CR1_CPOL;	//SCK is low in idle
-	SPI1->CR1 &=  ~SPI_CR1_CPHA;	//Capturing on falling edge
-	SPI1->CR1 &=  ~SPI_CR1_DFF;		//8 bit frame format
-	SPI1->CR1 &= ~SPI_CR1_LSBFIRST;	//MSB first
-*/
-
-		SPI_InitTypeDef spi;
-		spi.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
-		spi.SPI_Mode = SPI_Mode_Master;
-		spi.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-		spi.SPI_FirstBit = SPI_FirstBit_MSB;
-		spi.SPI_DataSize = SPI_DataSize_8b;
-		spi.SPI_NSS = SPI_NSS_Soft;
-		spi.SPI_CPOL = SPI_CPOL_Low;
-		spi.SPI_CPHA = SPI_CPHA_1Edge;
-		SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
-		SPI_Init(SPI1, &spi);
-		SPI_Cmd(SPI1, ENABLE);
+	SPI_InitTypeDef spi;
+	spi.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+	spi.SPI_Mode = SPI_Mode_Master;
+	spi.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	spi.SPI_FirstBit = SPI_FirstBit_MSB;
+	spi.SPI_DataSize = SPI_DataSize_8b;
+	spi.SPI_NSS = SPI_NSS_Soft;
+	spi.SPI_CPOL = SPI_CPOL_Low;
+	spi.SPI_CPHA = SPI_CPHA_1Edge;
+	SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
+	SPI_Init(SPI1, &spi);
+	SPI_Cmd(SPI1, ENABLE);
 
 	//SPI->CR2 |= SPI_CR2_TXEIE;		//TX buffer empty interrupt
 	//SPI->CR2 |= SPI_CR2_RXNEIE;		//RX buffer not empty interrupt
-
-	//SPI->CR2 |= SPI_CR2_TXDMAEN;	//TX buffer DMA enable
-	//SPI->CR2 |= SPI_CR2_RXDMAEN;	//RX buffer DMA enable
 }
 
 
-void SPI_enable(SPI_TypeDef *SPI)
+void SPI1_enable(void)
 {
-	SPI->CR1 |= SPI_CR1_SPE;
+	SPI1->CR1 |= SPI_CR1_SPE;
 }
 
 
-void SPI_disable(SPI_TypeDef *SPI)
+void SPI1_disable(void)
 {
-	while(!(SPI->SR & SPI_SR_RXNE));
-	while(!(SPI->SR & SPI_SR_TXE));
-	while((SPI->SR & SPI_SR_BSY));
-	SPI->CR1 &= ~SPI_CR1_SPE;
+	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_RXNE, DISABLE);
+	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, DISABLE);
+	//while(!(SPI->SR & SPI_SR_RXNE));
+	//while(!(SPI->SR & SPI_SR_TXE));
+	while((SPI1->SR & SPI_SR_BSY));
+	SPI1->CR1 &= ~SPI_CR1_SPE;
 }
 
 
-void SPI_write(SPI_TypeDef *SPI, uint8_t data)
+void SPI1_write(uint8_t data)
 {
-	while(SPI->SR & SPI_SR_BSY);
-	SPI->DR = data;
+	while(SPI1->SR & SPI_SR_BSY);
+	SPI1->DR = data;
 }
 
 
-uint8_t SPI_read(SPI_TypeDef *SPI)
+bool SPI1_busy(void)
 {
-	return (uint8_t)(SPI->DR & 0xFF);
+	return	(SPI1->SR & SPI_SR_BSY);
 }
 
 
-bool SPI_busy(SPI_TypeDef *SPI)
+bool SPI1_TX_is_empty(void)
 {
-	return	(SPI->SR & SPI_SR_BSY);
+	return (SPI1->SR & SPI_SR_TXE);
 }
 
 
-bool SPI_TX_is_empty(SPI_TypeDef *SPI)
+bool SPI_RX_not_empty(void)
 {
-	return (SPI->SR & SPI_SR_TXE);
+	return (SPI1->SR & SPI_SR_RXNE);
 }
 
 
-bool SPI_RX_not_empty(SPI_TypeDef *SPI)
+uint8_t SPI1_available(void)
 {
-	return (SPI->SR & SPI_SR_RXNE);
+	return rx_counter;
+}
+
+/*
+ * *************************************
+ * !!!Check for available data first!!!
+ * *************************************
+ */
+uint16_t SPI1_read(void)
+{
+	uint16_t data = 0;
+	if(rx_counter > 0)
+	{
+		data = spi_rx_buffer[rx_rd_pointer];
+		rx_rd_pointer = (rx_rd_pointer == SPI1_RX_BUFFER_SIZE - 1) ? 0 : rx_rd_pointer+1;
+		rx_counter -= 1;
+	}
+
+	return data;
+}
+
+
+bool SPI1_tx_array(uint16_t *data, uint8_t len)
+{
+	if(tx_counter + len < SPI1_TX_BUFFER_SIZE)
+	{
+		for(uint32_t i = 0; i<len; i++)
+		{
+			spi_tx_buffer[tx_wr_pointer] = data[i];
+			tx_wr_pointer = (tx_wr_pointer == SPI1_TX_BUFFER_SIZE - 1) ? 0 : tx_wr_pointer+1;
+		}
+
+		tx_counter += len;
+		SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE);
+
+		return true;
+	}
+
+	return false;
+}
+
+
+
+void SPI1_IRQHandler(void)
+{
+	if(SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_RXNE))
+	{
+		spi_rx_buffer[rx_wr_pointer] =  SPI_I2S_ReceiveData(SPI1);
+		rx_wr_pointer = (rx_wr_pointer == SPI1_RX_BUFFER_SIZE - 1) ? 0 : rx_wr_pointer+1;
+		rx_counter++;
+
+		SPI_I2S_ClearITPendingBit(SPI1, SPI_I2S_IT_RXNE);
+	}
+
+	if(SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_TXE))
+	{
+		if(tx_counter > 0)
+		{
+			SPI_I2S_SendData(SPI1, spi_tx_buffer[tx_rd_pointer]);
+			tx_rd_pointer = (tx_rd_pointer == SPI1_TX_BUFFER_SIZE - 1) ? 0 : tx_rd_pointer;
+			tx_counter--;
+
+			SPI_I2S_ClearITPendingBit(SPI1, SPI_I2S_IT_TXE);
+		}
+		else
+		{
+			SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, DISABLE);
+		}
+	}
+
+	if(SPI_I2S_GetITStatus(SPI1, SPI_I2S_IT_OVR))
+	{
+		//overrun error
+		SPI_I2S_ClearITPendingBit(SPI1, SPI_I2S_IT_OVR);
+	}
+
+
 }
--- src/periph/spi.h
+++ src/periph/spi.h
@@ -11,14 +11,17 @@
 #include "stm32f10x.h"
 #include <stdbool.h>
 
-void 	SPI_init(SPI_TypeDef *SPI);
-void 	SPI_enable(SPI_TypeDef *SPI);
-void 	SPI_disable(SPI_TypeDef *SPI);
-void 	SPI_write(SPI_TypeDef *SPI, uint8_t data);
-uint8_t SPI_read(SPI_TypeDef *SPI);
-bool 	SPI_busy(SPI_TypeDef *SPI);
-bool	SPI_TX_is_empty(SPI_TypeDef *SPI);
-bool	SPI_RX_not_empty(SPI_TypeDef *SPI);
+void 	SPI1_init(void);
+void 	SPI1_enable(void);
+void 	SPI1_disable(void);
+void 	SPI1_write(uint8_t data);
+bool 	SPI1_busy(void);
+bool	SPI1_TX_is_empty(void);
+bool	SPI1_RX_not_empty(void);
+
+uint8_t		SPI1_available(void);
+bool 		SPI1_tx_array(uint16_t *data, uint8_t len);
+uint16_t	SPI1_read(void);
 
 
 #endif /* PERIPH_SPI_H_ */
