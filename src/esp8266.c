ae71e5ce5ade1ee42d67f14df89df90b845c5cab
kripton <brazzers1969@gmail.com>
2016-03-06 22:58:00 +0300

    esp on usart3

--- src/esp8266.c
+++ src/esp8266.c
@@ -6,42 +6,43 @@
  */
 #include "esp8266.h"
 #include "periph/usart.h"
-#include "periph/usart1.h"
+#include "periph/usart3.h"
 #include "avr_flasher.h"
 
+#define ESP_USART USART3
 
 bool ESP8266_SendData(uint8_t *data, uint8_t len)
 {
-	USART_SendArray(USART1, data, len);
+	USART3_tx_array(data, len);
 	return true;
 }
 
 
 bool ESP8266_SendAck(void)
 {
-	const uint8_t ack[4] = {ACK_PACKET_BYTE, ACK_PACKET_BYTE, ACK_PACKET_BYTE, ACK_PACKET_BYTE};
-	USART_SendArray(USART1, ack, 4);
+	uint8_t ack[4] = {ACK_PACKET_BYTE, ACK_PACKET_BYTE, ACK_PACKET_BYTE, ACK_PACKET_BYTE};
+	USART3_tx_array(ack, 4);
 	return true;
 }
 
 
 bool ESP8266_SendError(void)
 {
-	const uint8_t err[4] = {ERROR_PACKET_BYTE, ERROR_PACKET_BYTE, ERROR_PACKET_BYTE, ERROR_PACKET_BYTE};
-	USART_SendArray(USART1, err, 4);
+	uint8_t err[4] = {ERROR_PACKET_BYTE, ERROR_PACKET_BYTE, ERROR_PACKET_BYTE, ERROR_PACKET_BYTE};
+	USART3_tx_array(err, 4);
 	return true;
 }
 
 
 uint32_t ESP8266_available(void)
 {
-	return USART1_available();
+	return USART3_available();
 }
 
 
 uint8_t ESP8266_read(void)
 {
-	return USART1_read();
+	return USART3_read();
 }
 
 
