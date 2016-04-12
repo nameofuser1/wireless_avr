fe4ce104be155200badb7b973ad70e6d387932b4
kripton <brazzers1969@gmail.com>
2016-04-13 01:13:32 +0300

    Transmission check and last packet

--- src/esp8266.c
+++ src/esp8266.c
@@ -5,18 +5,68 @@
  *      Author: kripton
  */
 #include "esp8266.h"
-#include <stddef.h>
+#include "periph/usart.h"
 #include "periph/usart3.h"
 #include "PacketManager.h"
+#include <stddef.h>
 #include <stdio.h>
-#include "periph/usart.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+static Packet last_packet;
+
+#define ESP_STATUS_GPIO 		GPIOB
+#define ESP_STATUS_GPIO_IDR		GPIO_IDR_IDR4
+
+#define ESP_STATUS_GPIO_READ()	(ESP_STATUS_GPIO->IDR & ESP_STATUS_GPIO_IDR)
+
+
+void ESP8266_init(void)
+{
+	USART3_init();
+    USART_Cmd(USART3, ENABLE);
+	NVIC_EnableIRQ(USART3_IRQn);
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+}
+
+
+void ESP8266_WaitForReady(void)
+{
+	for(volatile uint32_t i=0; i<2000000; i++);
+	while(!ESP_STATUS_GPIO_READ())
+	{
+		for(volatile uint32_t i=0; i<1000000; i++);
+	}
+}
 
 
 bool ESP8266_SendPacket(Packet packet)
 {
 	if(packet.type != NONE_PACKET)
 	{
+
+		if(packet.data != last_packet.data)
+		{
+			if(last_packet.data != NULL)
+			{
+				free(last_packet.data);
+			}
+
+			last_packet.data = (uint8_t*)malloc(sizeof(uint8_t)*packet.data_length);
+			memcpy(last_packet.data, packet.data, packet.data_length);
+			last_packet.data_length = packet.data_length;
+		}
+
+		printf("Sending packet: ");
+		for(uint16_t i=0; i<packet.data_length; i++)
+		{
+			printf("0x%02x ", packet.data[i]);
+		}
+		printf("\r\n");
+
 		USART3_tx_array(packet.data, packet.data_length);
+
 		return true;
 	}
 
@@ -27,16 +77,7 @@ bool ESP8266_SendPacket(Packet packet)
 bool ESP8266_SendAck(void)
 {
 	Packet ack_packet = PacketManager_create_packet(NULL, 0, ACK_PACKET);
-
-	printf("Sending ack\r\n");
-	for(uint32_t i=0; i<ack_packet.data_length; i++)
-	{
-		printf("0x%02x ", ack_packet.data[i]);
-	}
-	printf("\r\n");
-
 	bool res = ESP8266_SendPacket(ack_packet);
-
 	PacketManager_free(ack_packet);
 
 	return res;
@@ -58,6 +99,18 @@ bool ESP8266_SendError(uint8_t error)
 uint32_t ESP8266_available(void)
 {
 	return USART3_available();
+}
+
+
+bool ESP8266_SendLastPacket(void)
+{
+	return ESP8266_SendPacket(last_packet);
+}
+
+
+bool ESP8266_TransmissionStatus(void)
+{
+	return USART3_transmission_status();
 }
 
 
--- src/esp8266.h
+++ src/esp8266.h
@@ -12,9 +12,31 @@
 #include "PacketManager.h"
 #include <stdbool.h>
 
+void ESP8266_init(void);
+
+/*
+ * Loop until status GPIO is set
+ */
+void ESP8266_WaitForReady(void);
+
 bool ESP8266_SendPacket(Packet packet);
 bool ESP8266_SendAck(void);
 bool ESP8266_SendError(uint8_t error);
+
+/*
+ * If we have wrong packet we ask for it again.
+ */
+bool ESP8266_SendLastPacket(void);
+
+/*
+ * Return true is transmission is still pending
+ * And false otherwise
+ */
+bool ESP8266_TransmissionStatus(void);
+
+/*
+ * Loads ssid info into ESP
+ */
 bool ESP8266_LoadNetworkData(uint8_t *ssid, uint8_t ssid_len, uint8_t *pwd, uint8_t pwd_len);
 
 uint32_t ESP8266_available(void);
