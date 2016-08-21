5992ec517f177ad3a2ec63aa89cfb90cedcae292
kripton <kamnev.u1969@gmail.com>
2016-08-21 08:57:59 +0300

    Added cmsis usart driver

---
+++ cmsis_stm32f10/rte_driver/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * RTE_Components.h
+ *
+ *  Created on: Aug 21, 2016
+ *      Author: kript0n
+ */
+
+#ifndef CMSIS_STM32F10_RTE_DRIVER_RTE_COMPONENTS_H_
+#define CMSIS_STM32F10_RTE_DRIVER_RTE_COMPONENTS_H_
+
+//#define RTE_Drivers_USART0              /* Driver USART0 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+#define RTE_Drivers_USART2              /* Driver USART2 */
+#define RTE_Drivers_USART3              /* Driver USART3 */
+
+
+#endif /* CMSIS_STM32F10_RTE_DRIVER_RTE_COMPONENTS_H_ */
