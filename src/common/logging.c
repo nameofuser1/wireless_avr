9100ff64b4034b38f7c3b1db30449e68a725bde1
kripton <kamnev.u1969@gmail.com>
2016-08-26 17:31:55 +0300

    Fixed level comparation bug

--- src/common/logging.c
+++ src/common/logging.c
@@ -17,7 +17,7 @@ char *levels_names[LOG_INFO+1] = {"LOG_ERROR", "LOG_DEBUG", "LOG_WARNING", "LOG_
 
 void LOGGING_Log(const char *message, const LogLevel lvl)
 {
-	if(lvl < level)
+	if(lvl <= level)
 	{
 		printf("%s -- %s", levels_names[lvl], message);
 	}
